111c52cf672843fc0539983bbddb8b152728be0e
gusstavo007 <gustavo_149210@gmail.com>
2023-06-08 21:32:29 -0500

    configurar 3.5

--- src/services/chatgtp-service.js
+++ src/services/chatgtp-service.js
@@ -11,20 +11,24 @@ async function GetMessageChatGPT(text) {
             apiKey: process.env.OPENAI_API_KEY
         });
 
-
+        //    model: "text-davinci-003",
+        //     prompt: text,
         const openai = new OpenAIApi(configuration);
         const response = await openai.createCompletion({
 
-            model: "text-davinci-003",
-            prompt: text,
+            model: "gpt-3.5-turbo",
+            messages: [{ "role": "user", "content": text }],
+
             temperature: 0.7,
             max_tokens: 1000,
         });
-
+        console.error('<---------- GetMessageChatGPT ------->');
         console.log(response.data.choices);
+        console.error('<---------- GetMessageChatGPT ------->');
+        console.log(response.data);
 
-
-        return response.data.choices[0].text.trim();
+        return response;
+        //return response.data.choices[0].text.trim();
 
 
     } catch (error) {
